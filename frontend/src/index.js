d998007a9e0ba781a307e70a31444dcfc5567a09
Faith Johnson <faithpjohnson@gmail.com>
2022-05-17 13:24:16 -0600

    render course cards

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 
-import CourseFeed from './Pages/CourseFeed/CourseFeed'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 // import SyllabusForm from './components/SyllabusForm/SyllabusForm'
@@ -10,7 +9,6 @@ import { BrowserRouter } from 'react-router-dom'
 const appJsx = (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-    <CourseFeed />
   </BrowserRouter>
 )
 
